852ceed6dee35b7f72c53bba7303f5e17614fc66
Ressiaf <facurd91@gmail.com>
2022-10-21 21:28:40 -0300

    search route update

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ const morgan = require('morgan')
 // Routes import
 const usersRoute = require("./routes/users.js")
 
+//Configuration
 app.set( "port" , process.env.PORT || 3000) 
 
 // Middlewares
 app.use(morgan("tiny"))
 
 // Routes
-app.use("/users" , usersRoute )
+app.use( "/users" , usersRoute ) 
+app.use( "*" , ( req, res, next ) => {
+    res.status(404).send(" 404 - not found ")
+} )
 
 //Server
 app.listen(app.get("port") , ( )  =>  {
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const router = express.Router()
 
+// /users
 router.get( "/" , (req, res) => {
     res.send ("hola mundo , usuarios")
 } )
+
+// req.params va a ser igual al id del usuario 
+// facebook.com/1  => req.params.userid= 1 
+router.get( "/:userid" , (req, res) => {
+    console.log( req.params.userid ); 
+} )
+
 
 
 
